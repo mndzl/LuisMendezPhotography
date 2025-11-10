de024dc8c8adf9094ab98e8d12f98caf900aeb4c
Luis Méndez <mendezgla.56@gmail.com>
2025-11-09 23:09:10 -0500

    Changed base url to ''

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',
+  base: '',
   server: {
     proxy: {
       '/api': 'http://localhost:8000', // Proxies requests starting with /api to http://localhost:5000
