e17ce50054b7b16830091e746bc56f5822e8b63f
Luis Méndez <mendezgla.56@gmail.com>
2025-11-11 14:33:23 -0500

    modified API function view to generic class view to return all sessions

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getSessions, name="getSessions"),
+    path('', views.SessionsListCreate.as_view(), name="getSessions"),
     path('newsession/', views.SessionsListCreate.as_view(), name="newSession")
 ]
--- api/views.py
+++ api/views.py
@@ -5,14 +5,6 @@ from .serializers import SessionSerializer
 from rest_framework import generics
 
 
-@api_view(['GET'])
-def getSessions(request):
-    sessions = Session.objects.all()
-    serialized_sessions = SessionSerializer(sessions, many=True)
-
-    return Response(serialized_sessions.data)
-
-
 class SessionsListCreate(generics.ListCreateAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
