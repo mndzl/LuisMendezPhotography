d16d9d7abda6362b3d24396955415f6e2586a4b2
Luis Méndez <mendezgla.56@gmail.com>
2025-12-09 16:45:02 -0500

    refactored new session into sessions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import Sessions from "./components/Sessions/Sessions";
-import NewSession from "./components/Sessions/NewSession";
 import ViewSession from "./components/Sessions/ViewSession";
 import Clients from "./components/Clients/Clients";
 import Categories from "./components/Categories/Categories";
@@ -23,7 +22,6 @@ function App() {
             <Route path="/clients" element={<Clients />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/profile" element={<ViewProfile />} />
-            <Route path="/newsession" element={<NewSession />} />
             <Route path="/sessions/:sessionID" element={<ViewSession />} />
             <Route path="*" element={<Navigate to="/sessions" />} />
           </Routes>
--- frontend/src/components/Sessions/Sessions.tsx
+++ frontend/src/components/Sessions/Sessions.tsx
@@ -5,7 +5,7 @@
     - view status of last session (images backed up, editing, purchases available, closed)
     - view clients with information
 */
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { format } from "date-fns";
 import "./sessions.css";
 import { Link } from "react-router-dom";
@@ -15,6 +15,96 @@ function Sessions() {
   const [sessions, setSessions] = useState<any[]>([]);
   const [collapsedUpcoming, setCollapsedUpcoming] = useState(false);
   const [collapsedPrevious, setCollapsedPrevious] = useState(true);
+  const [formError, setFormError] = useState(false);
+  const [clientsList, setClientsList] = useState([
+    {
+      id: null,
+      first_name: "",
+      last_name: "",
+    },
+  ]);
+  const [categoriesList, setCategoriesList] = useState([
+    {
+      id: null,
+      name: "",
+    },
+  ]);
+  const [newSession, setNewSession] = useState({
+    title: "",
+    description: "",
+    location: "",
+    client: null,
+    category: null,
+    date: "",
+  });
+  const closeModalRef = useRef(null);
+
+  const fetchClients = async () => {
+    const endpoint = "/api/getclients/";
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+
+      setClientsList(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const fetchCategories = async () => {
+    const endpoint = "/api/getcategories/";
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+
+      setCategoriesList(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<any>) => {
+    setFormError(false);
+    const { name, value } = e.target;
+    setNewSession((values) => ({ ...values, [name]: value }));
+  };
+
+  const createSession = async () => {
+    newSession.date = new Date(newSession.date).toISOString();
+    const endpoint = "/api/newsession/";
+    console.log(JSON.stringify(newSession));
+    try {
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newSession),
+      });
+
+      if (!response.ok) throw new Error("Coult not create session.");
+
+      const result = await response.json();
+
+      setNewSession({
+        title: "",
+        description: "",
+        location: "",
+        client: null,
+        category: null,
+        date: "",
+      });
+
+      setFormError(false);
+
+      window.location.href = `/sessions/${result.id}`; // Redirect to new session page
+
+      // TODO: display global alert
+    } catch (err) {
+      console.log(err);
+      alert("There was an error creating the session.");
+    }
+  };
 
   function colorCategory(categoryName: string): string {
     const colors: Record<string, string> = {
@@ -23,6 +113,14 @@ function Sessions() {
 
     return colors[categoryName];
   }
+
+  const validateData = () => {
+    if (newSession.client == "-1" || newSession.category == "-1") {
+      setFormError(true);
+    } else {
+      createSession();
+    }
+  };
 
   function renderSessions(type: "upcoming" | "previous") {
     if (loading)
@@ -93,6 +191,9 @@ function Sessions() {
 
   useEffect(() => {
     fetchSessions();
+    fetchClients();
+    fetchCategories();
+    setLoading(false);
   }, []);
 
   async function fetchSessions() {
@@ -114,10 +215,156 @@ function Sessions() {
       {/* Main content */}
       <div className="sessions-header d-flex justify-content-between align-items-center mb-4">
         <h1>Sessions</h1>
-        <Link to="/newsession" className="btn btn-outline-primary">
+        <button
+          className="btn btn-outline-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#createClientModal"
+        >
           <i className="fa-solid fa-plus me-1"></i>
           Create
-        </Link>
+        </button>
+        {/* Add client modal */}
+        <div
+          className="modal fade"
+          id="createClientModal"
+          aria-labelledby="createClientModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="createClientModalLabel">
+                  Create New Session
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  ref={closeModalRef}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      aria-label="title"
+                      name="title"
+                      onChange={handleChange}
+                      value={newSession.title}
+                      placeholder="Ana & John's Wedding"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="sessionEmail">Description</label>
+                    <textarea
+                      className="form-control"
+                      onChange={handleChange}
+                      value={newSession.description}
+                      name="description"
+                      placeholder="Meeting at the mountains by sunset"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="sessionLocation">Location</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="sessionLocation"
+                      value={newSession.location}
+                      onChange={handleChange}
+                      name="location"
+                      placeholder="1123 Confused Ln, Kissimmee FL 34741"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="sessionClient">Client</label>
+                    <select
+                      id="sessionClient"
+                      className="form-control"
+                      name="client"
+                      value={newSession.client || ""}
+                      onChange={handleChange}
+                    >
+                      <option key="-1" value={"-1"}>
+                        Select
+                      </option>
+                      {clientsList.map((client) => (
+                        <option key={client.id} value={client.id || ""}>
+                          {client.first_name + " " + client.last_name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="sessionType">Type of Session</label>
+                    <select
+                      id="sessionType"
+                      className="form-control"
+                      name="category"
+                      value={newSession.category || ""}
+                      onChange={handleChange}
+                    >
+                      <option key="-1" value={"-1"}>
+                        Select
+                      </option>
+                      {categoriesList.map((category) => (
+                        <option key={category.id} value={category.id || "null"}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="sessionDate">Date</label>
+                    <input
+                      type="datetime-local"
+                      id="sessionDate"
+                      name="date"
+                      onChange={handleChange}
+                      className="form-control"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer d-flex align-items-center justify-content-between">
+                <div className="errors d-flex align-items-center">
+                  {formError && (
+                    <p className="text-danger mb-0">Please check fields.</p>
+                  )}
+                </div>
+                <div className="modal-controls">
+                  <button
+                    type="button"
+                    className="btn btn-outline-secondary me-1"
+                    data-bs-dismiss="modal"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    // data-bs-dismiss="modal"
+                    onClick={validateData}
+                    disabled={
+                      !newSession.title ||
+                      !newSession.description ||
+                      !newSession.location ||
+                      !newSession.client ||
+                      !newSession.category ||
+                      !newSession.date
+                    }
+                  >
+                    Create
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="sessions-upcoming mb-5">
