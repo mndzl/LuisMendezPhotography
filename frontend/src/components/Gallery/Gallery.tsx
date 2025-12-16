7d21999ff9bc7b5c5997c759e6b2af819f7ef685
Luis Méndez <mendezgla.56@gmail.com>
2025-12-15 23:22:14 -0500

    View image full size with modal

--- frontend/src/components/Gallery/Gallery.tsx
+++ frontend/src/components/Gallery/Gallery.tsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Backlink from "../Backlink";
 import Footer from "../Footer/Footer";
 import "./Gallery.css";
 
 function Gallery() {
   const [lightMode, setLightMode] = useState(true);
+  const [selectedImageURL, setSelectedImageURL] = useState(null);
+  const imageExpandModal = useRef<HTMLDialogElement | null>(null);
+
+  const expandImage = (e: React.ChangeEvent<any>) => {
+    setSelectedImageURL(e.target.src);
+    console.log(e.target.src);
+  };
+
+  useEffect(() => {
+    if (!selectedImageURL) {
+      imageExpandModal.current?.close();
+      document.body.style.overflow = "scroll";
+    } else {
+      imageExpandModal.current?.showModal();
+      document.body.style.overflow = "hidden";
+    }
+  }, [selectedImageURL]);
 
   return (
     <div className={`gallery m-0 ${lightMode ? "" : "bg-dark"}`}>
@@ -28,6 +45,16 @@ function Gallery() {
         <Backlink color={lightMode ? "#454545ff" : "#ffffffff"} />
       </div>
       <main className="w-100 p-0 h-100 d-flex justify-content-center">
+        <dialog ref={imageExpandModal} className="w-75 p-0 border-0">
+          <button
+            className="btn-close position-absolute top-0 end-0 mt-2 me-2 p-2 bg-light border rounded-3"
+            onClick={() => setSelectedImageURL(null)}
+          ></button>
+          {selectedImageURL && (
+            <img src={`${selectedImageURL}`} alt="" className="img-fluid" />
+          )}
+        </dialog>
+
         <div className="gallery" style={{ maxWidth: "90%" }}>
           <div className="gallery-info text-center p-0 py-5">
             <h1 className={`m-0 ${lightMode ? "" : "text-light"}`}>
@@ -43,79 +70,89 @@ function Gallery() {
                 src="/sports.jpeg"
                 alt="Hero Image"
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/wedding.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>{" "}
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/wedding.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>{" "}
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/sports.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/sports.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/sports.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/sports.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/wedding.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/sports.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
             <div className="gallery-img col-6 col-md-4 col-lg-3">
               <img
                 src="/wedding.jpeg"
-                alt="Hero Image"
+                alt=""
                 className="w-100 h-100 rounded"
-                style={{ objectFit: "cover" }}
+                style={{ objectFit: "cover", cursor: "pointer" }}
+                onClick={expandImage}
               />
             </div>
           </div>
